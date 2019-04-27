fe58f8bb01aeb3afe0cf225dc0b6de2fce87e380
Jake Carter <jake@cartermade.com>
2019-04-26 18:47:08 -0700

    Removed deepcompare helper.
    Turned out to not be very useful.

--- lib/libCirclesTest.lua
+++ lib/libCirclesTest.lua
@@ -1,28 +1,8 @@
-function deepcompare(t1,t2,ignore_mt)
-  local ty1 = type(t1)
-  local ty2 = type(t2)
-  if ty1 ~= ty2 then return false end
-  -- non-table types can be directly compared
-  if ty1 ~= 'table' and ty2 ~= 'table' then return t1 == t2 end
-  -- as well as tables which have the metamethod __eq
-  local mt = getmetatable(t1)
-  if not ignore_mt and mt and mt.__eq then return t1 == t2 end
-  for k1,v1 in pairs(t1) do
-    local v2 = t2[k1]
-    if v2 == nil or not deepcompare(v1,v2) then return false end
-  end
-  for k2,v2 in pairs(t2) do
-    local v1 = t1[k2]
-    if v1 == nil or not deepcompare(v1,v2) then return false end
-  end
-  return true
-end
-
 local libc = dofile('/Users/jake/Code/norns/circles/lib/libCircles.lua')
 
 -- test default values
 assert(libc ~= nil)
-assert(deepcompare(libc.p, {["x"] = 64, ["y"] = 32}))
+assert(libc.p.x == 64 and libc.p.y == 32)
 assert(#libc._circles == 0)
 
 -- test add circle with default values
@@ -46,7 +26,7 @@ assert(libc._circles[1].y == 20)
 
 -- test reset
 libc._reset()
-assert(deepcompare(libc.p, {["x"] = 64, ["y"] = 32}))
+assert(libc.p.x == 64 and libc.p.y == 32)
 assert(#libc._circles == 0)
 
 -- test burst
